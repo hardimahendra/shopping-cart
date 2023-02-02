220018b1b161d2bf6a739d392df5348306213f42
Hardi Mahendra <41864461+hardimahendra@users.noreply.github.com>
2023-02-02 13:52:29 +0700

    add css

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,12 +3,14 @@ import {Link} from 'react-router-dom'
 export function Home() {
   return (
     <>
-      <h1>Hardi Mahendra</h1>
-        <button>          
-          <Link to="/store">
-            Go to Product
-          </Link>
-      </button>
+      <div className="container-home">
+        <h1>Shopping Tech</h1>
+        <button>
+            <Link to="/store">
+              Go to Product
+            </Link>
+        </button>
+      </div>
     </>
-  )
+  );
 }
